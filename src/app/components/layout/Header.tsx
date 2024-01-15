369cb9ae9a43946f7826df620908832f9632dc54
Murillo Nahás <murillonahas2003@gmail.com>
2024-01-15 09:09:35 -0300

    feat: header component

--- src/app/components/layout/Header.tsx
+++ src/app/components/layout/Header.tsx
@@ -5,7 +5,7 @@ export function Header() {
     <header className="">
       <Image src="./Nahas.svg" alt="Me" width="35" height="35" />
       <p className="mt-3 font-medium">Murillo Nahas</p>
-      <span>Software Engineer</span>
+      <span>Software Engineer</span> <br />
       <span>São Paulo, Brazil</span>
     </header>
   );
