cab2eb774bd4364e28900dd90633aed666ad461e
Murillo Nahás <murillonahas2003@gmail.com>
2024-01-16 14:38:08 -0300

    feat: article component

---
+++ src/app/components/elements/Article.tsx
@@ -0,0 +1,24 @@
+import Link from "next/link";
+
+interface ArticleProps {
+  title: string;
+  date: string;
+  numberOfViews: number;
+  href: string;
+}
+
+export function Article({ title, date, numberOfViews, href }: ArticleProps) {
+  return (
+    <section className="mt-12">
+      <Link
+        href={href}
+        className="text-gray-950 decoration-gray-400 font-normal underline decoration-solid hover:decoration-gray-500"
+      >
+        {title}
+      </Link>
+      <p className="text-sm font-light text-gray-500">
+        {date} · {numberOfViews} views
+      </p>
+    </section>
+  );
+}
