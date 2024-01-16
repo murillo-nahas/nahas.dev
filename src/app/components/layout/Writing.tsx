674ca9d6e1a98d9519ace10a6c3cf509d5ac7029
Murillo Nahás <murillonahas2003@gmail.com>
2024-01-16 10:36:44 -0300

    feat: writing component

--- src/app/components/layout/Writing.tsx
+++ src/app/components/layout/Writing.tsx
@@ -1,7 +1,29 @@
+import Link from "next/link";
+
 export function Writing() {
   return (
-    <section>
-      <p></p>
+    <section className="mt-20">
+      <h2 className="font-medium text-gray-950">Writing</h2>
+      <div className="mt-6">
+        <Link
+          className="text-gray-950 decoration-gray-400 font-medium underline decoration-solid hover:decoration-gray-500"
+          href={"blog"}
+        >
+          My tech plans for 2024
+        </Link>
+        <p className="text-sm text-stone-700 leading-7">
+          January 20, 2024 · 0 views
+        </p>
+      </div>
+      <p className="mt-12">
+        Read more{" "}
+        <Link
+          href={"blog"}
+          className="text-gray-950 decoration-gray-400 font-medium underline decoration-solid hover:decoration-gray-500"
+        >
+          blog posts
+        </Link>{" "}
+      </p>
     </section>
   );
 }
