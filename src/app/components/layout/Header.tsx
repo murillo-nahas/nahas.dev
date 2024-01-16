65fdb2704fa4a1b0c1e1e3d0aaa7de206374634f
Murillo Nahás <murillonahas2003@gmail.com>
2024-01-15 22:19:41 -0300

    feat: finishing header component

--- src/app/components/layout/Header.tsx
+++ src/app/components/layout/Header.tsx
@@ -3,10 +3,10 @@ import Image from "next/image";
 export function Header() {
   return (
     <header className="">
-      <Image src="./Nahas.svg" alt="Me" width="35" height="35" />
-      <p className="mt-3 font-medium">Murillo Nahas</p>
-      <span>Software Engineer</span> <br />
-      <span>São Paulo, Brazil</span>
+      <Image src="./Nahas.svg" alt="Me" width="50" height="50" />
+      <p className="mt-3 font-medium">Murillo Nahás</p>
+      <span className="mt-1 text-gray-800">Software Engineer</span> <br />
+      <span className="mt-1 text-gray-800">São Paulo, Brazil</span>
     </header>
   );
 }
