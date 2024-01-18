79d3b467923c6a759c6923d5eb8790ad2c3df05b
Murillo Nahás <murillonahas2003@gmail.com>
2024-01-17 21:37:45 -0300

    feat: contact component

---
+++ src/app/components/layout/Contact/Contact.tsx
@@ -0,0 +1,52 @@
+"use client";
+
+import React, { useState } from "react";
+import { IoMdClose } from "react-icons/io";
+
+interface ContactProps {
+  isOpen: boolean;
+  closeModal: () => void;
+}
+
+export function Contact({ isOpen, closeModal }: ContactProps) {
+  return (
+    <>
+      {isOpen && (
+        <div className="fixed inset-0 bg-slate-100 bg-opacity-50 flex items-center justify-center">
+          <div className="p-8 shadow-lg w-3/12 h-auto bg-white rounded-md flex flex-col items-start justify-center">
+            <div className="w-full flex justify-end">
+              <IoMdClose
+                onClick={closeModal}
+                className="text-neutral-600 hover:text-neutral-400 cursor-pointer"
+              />
+            </div>
+            <form className="w-full mt-8 flex flex-col items-center justify-center">
+              <div className="w-full flex flex-col flex-start justify-center">
+                <label>Email</label>
+                <input
+                  type="email"
+                  placeholder="johndoe@example.com"
+                  className="rounded-md outline-gray-200 border border-slate-300 mt-1 h-12 p-2"
+                />
+              </div>
+              <div className="mt-8 w-full flex flex-col flex-start justify-center">
+                <label>Message</label>
+                <textarea
+                  rows={10}
+                  placeholder="Hi Murillo, I have a question about..."
+                  className="rounded-md outline-gray-200 border border-slate-300 mt-1 p-2 "
+                ></textarea>
+              </div>
+              <button
+                type="submit"
+                className="cursor-pointer text-sm p-2 mt-8 w-full bg-neutral-600 hover:bg-neutral-400 rounded-md text-white font-bold"
+              >
+                Submit
+              </button>
+            </form>
+          </div>
+        </div>
+      )}
+    </>
+  );
+}
