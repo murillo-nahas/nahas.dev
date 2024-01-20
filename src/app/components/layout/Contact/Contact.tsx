ba595d8b93ffced9b85c2262058f841b48ea26f4
Murillo Nahás <murillonahas2003@gmail.com>
2024-01-20 01:32:54 -0300

    feat: using .env variables

--- src/app/components/layout/Contact/Contact.tsx
+++ src/app/components/layout/Contact/Contact.tsx
@@ -1,7 +1,9 @@
 "use client";
 
-import React, { useState } from "react";
+import React, { useEffect, useRef } from "react";
 import { IoMdClose } from "react-icons/io";
+
+import emailjs from "@emailjs/browser";
 
 interface ContactProps {
   isOpen: boolean;
@@ -9,6 +11,31 @@ interface ContactProps {
 }
 
 export function Contact({ isOpen, closeModal }: ContactProps) {
+  const form = useRef(null);
+
+  function sendEmail(event: any) {
+    event.preventDefault();
+
+    const currentForm = form.current;
+    if (currentForm == null) return;
+
+    emailjs
+      .sendForm(
+        process.env.NEXT_PUBLIC_SERVICE_ID ?? "",
+        process.env.NEXT_PUBLIC_TEMPLATE_ID ?? "",
+        currentForm,
+        process.env.NEXT_PUBLIC_PUBLIC_KEY ?? ""
+      )
+      .then(
+        (result) => {
+          console.log(result.text);
+        },
+        (error) => {
+          console.log(error.text);
+        }
+      );
+  }
+
   return (
     <>
       {isOpen && (
@@ -20,13 +47,18 @@ export function Contact({ isOpen, closeModal }: ContactProps) {
                 className="text-neutral-600 hover:text-neutral-400 cursor-pointer"
               />
             </div>
-            <form className="w-full mt-8 flex flex-col items-center justify-center">
+            <form
+              ref={form}
+              onSubmit={sendEmail}
+              className="w-full mt-8 flex flex-col items-center justify-center"
+            >
               <div className="w-full flex flex-col flex-start justify-center">
                 <label>Email</label>
                 <input
                   type="email"
                   placeholder="johndoe@example.com"
                   className="rounded-md outline-gray-200 border border-slate-300 mt-1 h-12 p-2"
+                  name="user_email"
                 />
               </div>
               <div className="mt-8 w-full flex flex-col flex-start justify-center">
@@ -35,10 +67,12 @@ export function Contact({ isOpen, closeModal }: ContactProps) {
                   rows={10}
                   placeholder="Hi Murillo, I have a question about..."
                   className="rounded-md outline-gray-200 border border-slate-300 mt-1 p-2 "
+                  name="message"
                 ></textarea>
               </div>
               <button
                 type="submit"
+                value="Send"
                 className="cursor-pointer text-sm p-2 mt-8 w-full bg-neutral-600 hover:bg-neutral-400 rounded-md text-white font-bold"
               >
                 Submit
