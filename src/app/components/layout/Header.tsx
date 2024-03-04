c08a0cbce1aa67d01f932c130c082fed01d65aef
murillo.nahas <murillonahas@hotmail.com>
2024-03-04 20:32:15 -0300

    feat: header component

--- src/app/components/layout/Header.tsx
+++ src/app/components/layout/Header.tsx
@@ -2,11 +2,12 @@ import Image from "next/image";
 
 export function Header() {
   return (
-    <header className="">
+    <header>
       <Image src="./Nahas.svg" alt="Me" width="50" height="50" />
-      <p className="mt-3 font-medium">Murillo Nahás</p>
-      <span className="mt-1 text-gray-800">Software Engineer</span> <br />
-      <span className="mt-1 text-gray-800">São Paulo, Brazil</span>
+      <p className="mt-3 font-medium text-textGray">Murillo Nahás</p>
+      <span className="mt-1 text-textGrayDefault">Software Engineer</span>{" "}
+      <br />
+      <span className="mt-1 text-textGrayDefault">São Paulo, Brazil</span>
     </header>
   );
 }
