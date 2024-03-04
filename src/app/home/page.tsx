7383dd4940950620dce1678e9433dd7e70030e01
murillo.nahas <murillonahas@hotmail.com>
2024-03-04 20:32:24 -0300

    feat: home page

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -1,21 +1,92 @@
-import { About } from "../components/layout/About";
-import { Footer } from "../components/layout/Footer";
-import { Header } from "../components/layout/Header";
-import { Work } from "../components/layout/Work";
-import { Writing } from "../components/layout/Writing";
+"use client";
 
-function Home() {
+import Link from "next/link";
+import { Header } from "../components/layout/Header";
+import { useState } from "react";
+import { Contact } from "../components/layout/Contact/Contact";
+import { Footer } from "../components/layout/Footer";
+import React from "react";
+
+const pagesNavigation = [
+  {
+    name: "Writing",
+    url: "/writing",
+  },
+  {
+    name: "About",
+    url: "/about",
+  },
+  {
+    name: "Work",
+    url: "/work",
+  },
+];
+
+export default function Home() {
+  const [isModalOpen, setModalOpen] = useState(false);
+
+  function handleOpenModal() {
+    setModalOpen(true);
+  }
+
+  function handleCloseModal() {
+    setModalOpen(false);
+  }
+
   return (
     <>
-      <main className="w-2/6 m-auto h-auto">
+      <main className="w-3/6 m-auto h-auto">
         <Header />
-        <About />
-        <Work />
-        <Writing />
+
+        <p className="mt-16 text-textGrayDefault">
+          I am a Software Engineer from Brazil, creating awesome projects and
+          sharing knowledge over the internet. Focused mostly of the time on web
+          development, I love to see things being created from scratch.{" "}
+        </p>
+
+        <p className="mt-8 text-textGrayDefault">
+          You can read more about me on&nbsp;
+          <Link
+            href={"/about"}
+            className="font-medium underline hover:text-textGrayDefaultHover"
+          >
+            about
+          </Link>
+          &nbsp;page.
+        </p>
+
+        <nav className="mt-8">
+          {pagesNavigation.map((el, index) => (
+            <React.Fragment key={el.name}>
+              <Link
+                href={el.url}
+                className="text-textGrayDefault hover:text-textGrayDefaultHover hover:underline"
+              >
+                {el.name}
+              </Link>
+              {index !== pagesNavigation.length - 1 && (
+                <span>&nbsp;·&nbsp;</span>
+              )}
+            </React.Fragment>
+          ))}
+        </nav>
+
+        <p className="mt-8 text-textGrayDefault">
+          Feel free to mail me by&nbsp;
+          <span
+            onClick={handleOpenModal}
+            className="cursor-pointer text-textGrayDefault hover:text-textGrayDefaultHover font-medium underline"
+          >
+            clicking here
+          </span>
+        </p>
+
+        <div className="mt-8 w-full border-b border-textGrayDefault rounded-md"></div>
+
+        <Footer />
+
+        <Contact isOpen={isModalOpen} closeModal={handleCloseModal} />
       </main>
-      <Footer />
     </>
   );
 }
-
-export default Home;
