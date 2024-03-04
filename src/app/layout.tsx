82decdaf322daa56493c2a01b6bf67984ab58e49
murillo.nahas <murillonahas@hotmail.com>
2024-03-04 20:32:00 -0300

    feat: change app meta tag

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,7 +5,7 @@ import "./index.css";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Murillo Nahas, Software Engineer",
+  title: "Murillo Nahás",
   description: "Murillo Nahas's Blog",
 };
 
