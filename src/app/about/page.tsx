6c5858958f90774132c7547a5747093d8362761b
murillo.nahas <murillonahas@hotmail.com>
2024-03-04 21:12:25 -0300

    feat: wip about me

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,5 +1,24 @@
+import { Metadata } from "next";
+import { Return } from "../components/elements/Return";
+
+export const metadata: Metadata = {
+  title: "About me",
+};
+
 export default function About() {
-    return (
-        <h1> Under Development...</h1>
-    )
+  return (
+    <>
+      <main className="w-3/6 m-auto h-auto">
+        <p className="mt-32 font-medium text-textGray">About me</p>
+        <p className="mt-4 text-sm text-textGrayDefault">
+          A little about my history...
+        </p>
+
+        <p className="mt-16 font-medium text-sm text-textGray">
+          Under development...
+        </p>
+        <Return label="Back" href={"/home"} />
+      </main>
+    </>
+  );
 }
