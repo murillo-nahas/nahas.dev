0d8ccf0be3f4c1930e4267cd068d84771eb4601e
murillo.nahas <murillonahas@hotmail.com>
2024-03-04 21:12:03 -0300

    feat: add title metadata

--- src/app/writing/page.tsx
+++ src/app/writing/page.tsx
@@ -1,15 +1,24 @@
+import { Metadata } from "next";
 import { Return } from "../components/elements/Return";
-import { Footer } from "../components/layout/Footer";
-import { Posts } from "../components/layout/Posts";
+
+export const metadata: Metadata = {
+  title: "Writing",
+};
 
 export default function Writing() {
   return (
     <>
       <main className="w-3/6 m-auto h-auto">
-        <Return label="Index" href={"/"} />
-        <Posts />
+        <p className="mt-32 font-medium text-textGray">Writing</p>
+        <p className="mt-4 text-sm text-textGrayDefault">
+          This is where my ideas come to life
+        </p>
+
+        <p className="mt-16 font-medium text-sm text-textGray">
+          Under development...
+        </p>
+        <Return label="Back" href={"/home"} />
       </main>
-      <Footer />
     </>
   );
 }
