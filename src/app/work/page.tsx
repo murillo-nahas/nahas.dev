018e8d900b2a076b4ecd7edce63caaeef3b03508
murillo.nahas <murillonahas@hotmail.com>
2024-03-04 21:12:07 -0300

    feat: add title metadata

---
+++ src/app/work/page.tsx
@@ -0,0 +1,24 @@
+import { Metadata } from "next";
+import { Return } from "../components/elements/Return";
+
+export const metadata: Metadata = {
+  title: "Work",
+};
+
+export default function Work() {
+  return (
+    <>
+      <main className="w-3/6 m-auto h-auto">
+        <p className="mt-32 font-medium text-textGray">Work</p>
+        <p className="mt-4 text-sm text-textGrayDefault">
+          Where you can find more about my professional experience
+        </p>
+
+        <p className="mt-16 font-medium text-sm text-textGray">
+          Under development...
+        </p>
+        <Return label="Back" href={"/home"} />
+      </main>
+    </>
+  );
+}
