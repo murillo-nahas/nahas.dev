8a0396da8fd5c7868afdb7d2e5aad4e2f7b9411d
Murillo Nahas <murillonahas2003@gmail.com>
2026-02-13 10:51:30 -0300

    feat: created project card component

--- src/app/pages/home/components/project-card/project-card.ts
+++ src/app/pages/home/components/project-card/project-card.ts
@@ -1,8 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, input } from '@angular/core';
+import { TechBadge } from '../tech-badge/tech-badge';
+import { ExternalLink, LucideAngularModule } from 'lucide-angular';
 
 @Component({
   selector: 'app-project-card',
-  imports: [],
+  imports: [TechBadge, LucideAngularModule],
   templateUrl: './project-card.html',
 })
-export class ProjectCard { }
+export class ProjectCard {
+  readonly linkIcon = ExternalLink;
+
+  readonly name = input.required<string>();
+  readonly description = input.required<string>();
+  readonly link = input.required<string>();
+  readonly techs = input.required<string[]>();
+}
