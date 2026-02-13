677ca8370aee2866df6a9a61335da4650d208d16
Murillo Nahas <murillonahas2003@gmail.com>
2026-02-12 22:30:52 -0300

    feat(wip): home page

--- src/app/pages/home/home.ts
+++ src/app/pages/home/home.ts
@@ -1,8 +1,39 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
+import { Connect } from '../../shared/components/connect/connect';
+
+type Experience = {
+  title: string;
+  company: string;
+  date: string;
+  description: string;
+};
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  imports: [RouterLink, Connect],
   templateUrl: './home.html',
 })
-export class Home { }
+export class Home {
+
+  readonly experience: Experience[] = [
+    {
+      title: 'Full-Stack Software Engineer',
+      company: 'Merch',
+      date: '2024 · Present',
+      description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quos.Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quos.Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quos.',
+    },
+    {
+      title: 'Mobile Software Engineer',
+      company: 'Be220',
+      date: '2024 · 2025',
+      description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quos.Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quos.Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quos.',
+    },
+    {
+      title: 'Mobile Software Engineer',
+      company: 'Bradesco Seguros',
+      date: '2022 · 2024',
+      description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quos.Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quos.Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quos.',
+    },
+  ];
+}
