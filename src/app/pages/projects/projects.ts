a23dd19055f4d8dcd3d356148e8e3cff256e3ddb
Murillo Nahas <murillonahas2003@gmail.com>
2026-02-16 20:48:48 -0300

    feat: projects page

--- src/app/pages/projects/projects.ts
+++ src/app/pages/projects/projects.ts
@@ -1,8 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, computed, signal } from '@angular/core';
+import { PROJECTS } from '../../shared/consts/projects.const';
+import { ProjectFilter } from '../../shared/models/projects.model';
+import { NavigationBar } from './components/navigation-bar/navigation-bar';
+import { ProjectCard } from './components/project-card/project-card';
 
 @Component({
   selector: 'app-projects',
-  imports: [],
+  imports: [NavigationBar, ProjectCard],
   templateUrl: './projects.html',
 })
-export class Projects { }
+export class Projects {
+  readonly activeFilter = signal<ProjectFilter>(ProjectFilter.ALL);
+
+  readonly filteredProjects = computed(() => {
+    const filter = this.activeFilter();
+
+    if (filter === ProjectFilter.ALL) {
+      return PROJECTS;
+    }
+
+    return PROJECTS.filter((project) => project.tag === filter);
+  });
+
+  onFilterChange(filter: ProjectFilter): void {
+    this.activeFilter.set(filter);
+  }
+}
