d2ef7a40b160267bd2fbd870b4e2ceb0243cb687
Murillo Nahas <murillonahas2003@gmail.com>
2026-02-16 20:48:17 -0300

    feat: updated project model and created project enum

--- src/app/shared/consts/projects.const.ts
+++ src/app/shared/consts/projects.const.ts
@@ -1,16 +1,20 @@
-import { Project } from "../models/projects.model";
+import { Project, ProjectFilter } from "../models/projects.model";
 
 export const PROJECTS: Project[] = [
   {
     name: 'Jobtrackr frontend',
     description: 'Frontend infrastructure for Jobtrackr, a job tracking tool.',
+    date: '2026',
     link: 'https://github.com/murillo-nahas/jobtrackr-frontend',
     techs: ['React', 'TypeScript', 'Shadcn UI', 'Tanstack Query', 'TailwindCSS', 'Zod'],
+    tag: ProjectFilter.FRONTEND,
   },
   {
     name: 'Jobtrackr backend',
     description: 'Backend infrastructure for Jobtrackr, a job tracking tool.',
+    date: '2026',
     link: 'https://github.com/murillo-nahas/jobtrackr-backend',
     techs: ['NestJS', 'TypeScript', 'PostgreSQL', 'Prisma', 'Docker'],
+    tag: ProjectFilter.BACKEND,
   },
 ];
--- src/app/shared/models/projects.model.ts
+++ src/app/shared/models/projects.model.ts
@@ -1,6 +1,14 @@
+export enum ProjectFilter {
+  ALL = 'all',
+  FRONTEND = 'frontend',
+  BACKEND = 'backend',
+}
+
 export type Project = {
   name: string;
   description: string;
+  date: string;
   link: string;
   techs: string[];
+  tag: Exclude<ProjectFilter, ProjectFilter.ALL>;
 };
